e9f273a9baad3c0613ec3cae5d8cb865d73ada7c
vaidehi <vaidehi117v@gmail.com>
2024-12-20 00:33:58 -0500

    git action

--- app/settings.py
+++ app/settings.py
@@ -14,13 +14,11 @@ class Settings(BaseSettings):
     db_name: str
     db_port: Optional[int] = 5432  # Default PostgreSQL port
     salt: str  # Additional salt for password hashing
-    
-    class Config:
-        env_prefix = "TEST_"  # Change the prefix to "TEST_"
 
-    # class Config:
-    #     env_file = ".env"
-    #     env_prefix = "DB_"  # Prefix only for database-related environment variables
-    #     fields = {
-    #         'salt': {'env': 'SALT'},  # Map 'salt' to 'SALT' without the 'DB_' prefix
-    #     }
+
+    class Config:
+        env_file = ".env"
+        env_prefix = "DB_"  # Prefix only for database-related environment variables
+        fields = {
+            'salt': {'env': 'SALT'},  # Map 'salt' to 'SALT' without the 'DB_' prefix
+        }
